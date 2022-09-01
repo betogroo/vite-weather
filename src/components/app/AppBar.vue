f5130b15a357b106bd758b303ed523e091735ede
betogroo <luizhumberto@gmail.com>
2022-09-01 11:23:45 -0300

    refactor: remove navigation-drawer

--- src/components/app/AppBar.vue
+++ src/components/app/AppBar.vue
@@ -1,12 +1,24 @@
-<script setup lang="ts">
-defineEmits(['toggle-drawer'])
-</script>
+<script setup lang="ts"></script>
 <template>
   <v-app-bar color="primary">
-    <v-app-bar-nav-icon @click="$emit('toggle-drawer')"></v-app-bar-nav-icon>
-    <v-app-bar-title>Vue Vite Project</v-app-bar-title>
+    <v-spacer></v-spacer>
+    <h1 class="text-h6">
+      <router-link
+        class="text-white text-decoration-none"
+        :to="{ name: 'HomeView' }"
+      >
+        <v-icon>mdi-white-balance-sunny</v-icon>
+        O App do Tempo
+      </router-link>
+    </h1>
+    <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
     <v-btn icon>
-      <v-icon>mdi-dots-horizontal</v-icon>
+      <v-icon>mdi-plus</v-icon>
     </v-btn>
+    <v-btn icon>
+      <v-icon>mdi-information-variant</v-icon>
+    </v-btn>
+    <v-spacer></v-spacer>
   </v-app-bar>
 </template>
